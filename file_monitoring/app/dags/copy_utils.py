ed567fcfb3806e5ebc370782debc6eac086bfc45
tunsmm <dragon553180@gmail.com>
2024-11-23 09:04:53 +0000

    change copy_utils

--- file_monitoring/app/dags/copy_utils.py
+++ file_monitoring/app/dags/copy_utils.py
@@ -4,10 +4,12 @@ import uuid
 import zlib
 from concurrent.futures import ThreadPoolExecutor
 
+from sqlalchemy.orm import Session
+
 from config.logger import logger
 from config.settings import NUMBER_OF_WORKERS_FOR_COPY_FILES
-
-from db_utils import create_tables_in_db
+from db_utils import create_sa_engine_to_main_db, create_tables_in_db
+from services.db_operations.file_copy_history import create_file_copy_history, update_file_copy_history
 
 create_tables_in_db()
 
@@ -57,23 +59,34 @@ def copy_file(unique_id: uuid.UUID, local_dir: str, remote_path: str, fname: str
     local_path = os.path.join(local_dir, fname)
     remote_path = os.path.join(remote_path, fname)
     
+    # Получаем размер файла
+    file_size = os.path.getsize(local_path)
+    size_in_megabytes_binary = round(file_size / (1024 ** 2), 2)
+    logger.info(f'{fname} file_size is {size_in_megabytes_binary} MB')
+
     # Calculate the checksum of the local file
     local_checksum = calculate_adler32(local_path)
-    
-    for attempt in range(retries):
-        copy_file_to_server_via_scp(local_path, remote_path)
-        
-        # Calculate the checksum of the remote file
-        # Здесь предполагается, что возможно получить доступ к файлу с текущего сервера
-        remote_checksum = calculate_adler32(remote_path)
 
-        if local_checksum == remote_checksum:
-            logger.info(f"{unique_id} | File copied successfully: {fname} (Attempt {attempt + 1})")
-            return fname
-        logger.info(f"Checksum mismatch for {fname} (Attempt {attempt + 1}). Retrying...")
-    
+    with create_sa_engine_to_main_db() as engine:
+        with Session(engine) as session:
+            # Создаем запись в истории копирования
+            entry_id = create_file_copy_history(session, fname, file_size, str(local_checksum))
+
+            for attempt in range(retries):
+                copy_file_to_server_via_scp(local_path, remote_path)
+
+                # Calculate the checksum of the remote file
+                remote_checksum = calculate_adler32(remote_path)
+
+                if local_checksum == remote_checksum:
+                    logger.info(f"{unique_id} | File copied successfully: {fname} (Attempt {attempt + 1})")
+                    # Обновляем запись в истории копирования
+                    update_file_copy_history(session, entry_id, str(remote_checksum))
+                    return fname
+
+                logger.info(f"Checksum mismatch for {fname} (Attempt {attempt + 1}). Retrying...")
+
     raise Exception(f"Failed to copy file {fname} after {retries} attempts.")
-
 
 def copy_file_to_server_via_scp(local_path: str, remote_path: str):
     """
