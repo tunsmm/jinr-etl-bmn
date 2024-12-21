42dbcd82bb84de7c827e523a981f47c3d6cdba4d
tunsmm <dragon553180@gmail.com>
2024-12-21 19:47:27 +0000

    fix errors, add logs

--- app/telegram_bot/model.py
+++ app/telegram_bot/model.py
@@ -3,8 +3,11 @@ import json
 
 from aiogram import Bot, Dispatcher, types
 
+from config.logger import logger
+
+
 class TelegramBot:
-    def __init__(self, token, allowed_users: set = set()):
+    def __init__(self, token, allowed_users: set = None):
         self.token = token
         if isinstance(allowed_users, set): 
             self.allowed_users = allowed_users
@@ -13,55 +16,55 @@ class TelegramBot:
         self.bot = Bot(token=self.token)
         self.dp = Dispatcher()
 
-        # Регистрация обработчиков
+        # Register handlers
         self.dp.message.register(self.handle_message)
 
     def load_users(self):
-        """Загружает идентификаторы пользователей из файла."""
+        """Load user telegram IDs from a file."""
+        file_name = './telegram_bot/users.json'
         try:
-            with open('users.json', 'r') as f:
+            with open(file_name, 'r') as f:
                 self.allowed_users = set(json.load(f))
+                logger.info(f'allowed_users from {file_name} is {self.allowed_users}')
         except FileNotFoundError:
+            logger.error(f'{file_name} is not found')
             self.allowed_users = set()
 
     def save_user(self, user_id):
-        """Сохраняет идентификатор пользователя в файл."""
         self.allowed_users.add(user_id)
         with open('users.json', 'w') as f:
             json.dump(list(self.allowed_users), f)
 
     async def send_message_to_all(self, message, file=None):
-        """Отправляет сообщение всем пользователям."""
         for user_id in self.allowed_users:
             await self.send_message(user_id, message, file)
 
     async def send_message(self, chat_id, message, file=None):
-        """Отправляет сообщение пользователю."""
         try:
             await self.bot.send_message(chat_id=chat_id, text=message)
             if file:
                 await self.bot.send_document(chat_id=chat_id, document=file)
-            print(f"Сообщение отправлено в {chat_id}: {message}")
+            logger.info(f"Message sent to {chat_id}: {message}")
         except Exception as e:
-            print(f"Ошибка при отправке сообщения: {e}")
+            logger.info(f"Error sending message: {e}")
 
     async def handle_message(self, message: types.Message):
-        """Обрабатывает входящие сообщения."""
         user_id = message.from_user.id
         if user_id not in self.allowed_users:
-            self.save_user(user_id)  # Сохраняем пользователя при любом сообщении
-            await self.send_message(user_id, "Вы добавлены в список получателей.")
+            self.save_user(user_id)  # Save the user upon any message
+            await self.send_message(user_id, "You have been added to the recipient list.")
 
     async def periodic_message_sender(self, pipeline_method):
-        """Отправляет сообщение всем пользователям каждую минуту."""
+        """Send a message to all users every minute."""
         while True:
             for function in pipeline_method():
+                logger.info(f'start {function.__name__}')
                 message, file = function()
+                logger.info(f'{function.__name__} message is {message}; file is {file}')
                 if message:
                     await self.send_message_to_all(message, file)
 
-            await asyncio.sleep(60)
+            await asyncio.sleep(60*20)
 
     async def start_polling(self):
-        """Запускает поллинг."""
         await self.dp.start_polling(self.bot)
